c1fb9550a8cc1b52fdc9812fd166d8013f70b6bf
archibowl <archibowl@protonmail.com>
2022-09-12 20:17:12 +1000

    Saving progress

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
+let x = document.cookie;
 let playPause = true;
-function audio(){
-    if(playPause == true){
+function cookieAudio(){
+    if(x == "mute"){
         playPause = false;
         document.getElementById('bgMusic').muted = true;
         document.getElementById('musicBtn').classList.add('fa-volume-xmark');
@@ -12,3 +13,21 @@ function audio(){
         document.getElementById('musicBtn').classList.add('fa-volume-high');
     }
 }
+function audio(){
+    if(playPause == true){
+        playPause = false;
+        document.getElementById('bgMusic').muted = true;
+        document.getElementById('musicBtn').classList.add('fa-volume-xmark');
+        document.getElementById('musicBtn').classList.remove('fa-volume-high');
+        document.cookie = "mute";
+        x = document.cookie;
+    } else{
+        playPause = true;
+        document.getElementById('bgMusic').muted = false;
+        document.getElementById('musicBtn').classList.remove('fa-volume-xmark');
+        document.getElementById('musicBtn').classList.add('fa-volume-high');
+        document.cookie = "loud";
+        x = document.cookie;
+    }
+}
+
