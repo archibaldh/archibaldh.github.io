7eb78c0cf769b7645f41f55fdbd731f7c7b617a1
archibowl <archibowl@protonmail.com>
2022-12-19 01:57:01 +1100

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function contactMe(){
+    document.getElementById("modal1").style.display = "block";
+}
+function closeFn1(){
+    document.getElementById("modal1").style.display = "none";
+}
+function closeFn2(){
+    document.getElementById("modal2").style.display = "none";
+}
+function myBusiness(){
+    document.getElementById("modal2").style.display = "block";
+}
